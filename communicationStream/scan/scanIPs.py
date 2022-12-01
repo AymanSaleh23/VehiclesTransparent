a5235ce17fbbba85974b8a77c9c2a13ef5a10328
Ayman Saleh <ayman.saleh.zien@gmail.com>
2022-12-02 01:12:50 +0200

    Update scanIP no more spaghetti
    - Upgrade the code to be Object-Oriented rather than be Spaghetti
    - Code is suitable for Windows.
    - New update to enhance and be configurable with Linux-based systems.

--- communicationStream/scan/scanIPs.py
+++ communicationStream/scan/scanIPs.py
@@ -1,97 +1,108 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue Oct 18 18:55:17 2022
-
-@author: as292
+Updated on Tue Dec 2   1:00:00 2022
 """
-transIP = 2
-import subprocess, re
-# import module
-import os
+""" Import all Essintial Packages   """
+import subprocess, re, os
 
-
-AP_name = "TEdata3CCB39"
-AP_pass = "29902026"
-
-
-# function to establish a new connection
-def createNewConnection(name, SSID, password):
-    print ("Creating Access Point Connection", name, SSID, password, sep="\t")
-    config = """<?xml version=\"1.0\"?>
-<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
-    <name>"""+AP_name+"""</name>
-    <SSIDConfig>
-        <SSID>
-            <name>"""+AP_name+"""</name>
-        </SSID>
-    </SSIDConfig>
-    <connectionType>ESS</connectionType>
-    <connectionMode>auto</connectionMode>
-    <MSM>
-        <security>
-            <authEncryption>
-                <authentication>WPA2PSK</authentication>
-                <encryption>AES</encryption>
-                <useOneX>false</useOneX>
-            </authEncryption>
-            <sharedKey>
-                <keyType>passPhrase</keyType>
-                <protected>false</protected>
-                <keyMaterial>"""+AP_pass+"""</keyMaterial>
-            </sharedKey>
-        </security>
-    </MSM>
-</WLANProfile>"""
-    command = "netsh wlan add profile filename=\""+name+".xml\""+" interface=Wi-Fi"
-    with open(name+".xml", 'w') as file:
-        file.write(config)
-    os.system(command)
-    print (command)
- 
-# function to connect to a network   
-def connect(name, SSID):
-    command = "netsh wlan connect name=\""+AP_name+"\" ssid=\""+AP_name+"\" interface=Wi-Fi"
-    os.system(command)
-    print (command)
- 
-    
-createNewConnection(AP_name,AP_name,AP_pass)
-
-connect(AP_name,AP_pass)
-
-def Get_Host (x):
-    Dot_counter = 0
-    Pos_counter = 0
-    
-    for i in x:
-        if i == '.':
-            Dot_counter+= 1
-            
-        if Dot_counter == 3:
-            return (x[0:Pos_counter+1], x[Pos_counter+1:])
-            break
-        Pos_counter+=1
+"""  Create class named scanIP  """
+class scanIP:
+    """ Class Constructor with arguments nameAP, SSID, Password """
+    def __init__(self, nameAP='Almosalamy', ssid='Almosalamy', password='Alm0salamy2000'):
         
-Network, First_Host = Get_Host("192.168.1.2")
-Network, Last_Host = Get_Host("192.168.1.6")
-
-
-empty_string = ""
-counter = 0
-
-for i in range( int(First_Host), int(Last_Host )+1):
-    process = subprocess.getoutput("ping -n 1 "+Network+str(i))
-    empty_string += process
-    string_needed = re.compile(r"TTL=")
-    mo = string_needed.search(empty_string)
-    
-    try :
-        if mo.group()== "TTL=":
-            print(str(Network+str(i))+"\tHost found")
-    except :
-            print("\t\t\tHost not found")
+        print("scanIP object is created...")
         
-    empty_string=""
+        """ run the feature """
+        self.__call__(nameAP, ssid, password)
+        
+    
+    """ Function to establish a new connection  """"
+    def create_new_connection(self,nameAP='Almosalamy', ssid='Almosalamy', password='Alm0salamy2000'):
+        print ("Creating Access Point Connection", nameAP, ssid, password, sep="\t")
+        config = """
+        <?xml version=\"1.0\"?>
+            <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+                <name>"""+nameAP+"""</name>
+                <SSIDConfig>
+                    <SSID>
+                        <name>"""+nameAP+"""</name>
+                    </SSID>
+                </SSIDConfig>
+                <connectionType>ESS</connectionType>
+                <connectionMode>auto</connectionMode>
+                <MSM>
+                    <security>
+                        <authEncryption>
+                            <authentication>WPA2PSK</authentication>
+                            <encryption>AES</encryption>
+                            <useOneX>false</useOneX>
+                        </authEncryption>
+                        <sharedKey>
+                            <keyType>passPhrase</keyType>
+                            <protected>false</protected>
+                            <keyMaterial>"""+password+"""</keyMaterial>
+                        </sharedKey>
+                    </security>
+                </MSM>
+                </WLANProfile>"""
+        command = "netsh wlan add profile filename=\""+nameAP+".xml\""+" interface=Wi-Fi"
+        with open(nameAP+".xml", 'w') as file:
+            file.write(config)
+        os.system(command)
+        print (command)
 
-print ("Done")
+    """ Function to connect to a network   """
+    def connect(self,name, ssid):
+        command = "netsh wlan connect name=\""+name+"\" ssid=\""+ssid+"\" interface=Wi-Fi"
+        os.system(command)
+        print (command)
+     
+    """ Dunder function to enable object to be callable """
+    def __call__(self,nameAP, ssid, password):
+        self.create_new_connection(nameAP, ssid, password)
+        print ("Connection Created Succssfully...")
+        self.connect(nameAP, ssid)
+        print ("Connection Established...")
+        print ("Done")
+    
+    """ Function to get the IP, Network """
+    def get_host (self,x):
+        Dot_counter = 0
+        Pos_counter = 0
+        
+        for i in x:
+            if i == '.':
+                Dot_counter+= 1
+                
+            if Dot_counter == 3:
+                return (x[0:Pos_counter+1], x[Pos_counter+1:])
+                break
+            Pos_counter+=1
+ 
+    """ Function to get all available Hosts in the LAN  """
+    def check_ips(self,start_ip,end_ip):
+        empty_string = ""
+        counter = 0
+        Network, First_Host = self.get_host(start_ip)
+        Network, Last_Host = self.get_host(end_ip)
+        found = []
+        for i in range( int(First_Host), int(Last_Host )+1):
+            process = subprocess.getoutput("ping -n 1 "+Network+str(i))
+            empty_string += process
+            string_needed = re.compile(r"TTL=")
+            mo = string_needed.search(empty_string)
             
+            try :
+                if mo.group()== "TTL=":
+                    print(str(Network+str(i))+"\tHost found")
+                    found.append(str(Network+str(i)))
+            except :
+                    print(str(Network+str(i))+"\t\tHost not found")
+                
+            empty_string=""
+        return found
+
+""" Only for test using windows """
+s = scanIP("Almosalamy","Almosalamy","Alm0salamy2000")
+found = s.get_host_ip(start_ip="192.168.1.9",end_ip="192.168.1.13")
+print ("available IPs:",found)
