519f4caf565088911242c424728b7c6f7e757318
Ayman Saleh <ayman.saleh.zien@gmail.com>
2023-02-16 22:15:13 +0200

    [UPGRADE] Serial class to be command-driven. [SOLVED] Bugs when object initiation

--- Tools/Test_serial_USB_ِAsynch_receive.py
+++ Tools/Test_serial_USB_ِAsynch_receive.py
@@ -1,8 +1,11 @@
 """     Test Serial App """
-sc = SerialComm(port='COM4', baudrate=115200, timeout=1)
+sc = SerialComm(port='COM5', baudrate=115200, timeout=1)
 
 while True:
+    sc.serial_command = "1"
+    print(">>>From App:", sc.get_value())
+    sc.serial_command = "2"
     print(">>>From App:", sc.get_value())
     time.sleep(0.5)
-    print ("Any Other app!!!")
+    print("Any Other app!!!")
     time.sleep(2)
--- comlib/com_serial.py
+++ comlib/com_serial.py
@@ -2,44 +2,51 @@ import serial
 import time 
 
 class SerialComm:
-    def __init__(self, port="COM4", baudrate=9600, timeout=1):
-        self.port = port
-        self.baudrate = baudrate
-        self.timeout = timeout
+    def __init__(self, port="COM4", parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+                 bytesize=serial.EIGHTBITS, baudrate=9600, timeout=1):
+        self.parity, self.stopbits = parity, stopbits
+        self.bytesize, self.port = bytesize, port
+        self.baudrate, self.timeout = baudrate, timeout
+
+        self.serial_command = "0"
         self.connection_state = False
         try:
-            self.ser = serial.Serial(self.port, self.baudrate, timeout=self.timeout)
-            self.ser.setDTR(False)
-            time.sleep(1)
-            self.ser.flushInput()
-            self.ser.setDTR(True)
-            time.sleep(2)
-            self.ser.reset_input_buffer()
-            self.connection_state = True
-            self.received_data = None
-            print("Serial Initialized")
+            self.ser = serial.Serial(self.port, self.baudrate, timeout=self.timeout, parity=self.parity,
+                                     stopbits=self.stopbits, bytesize=self.bytesize)
+            
+            if not self.ser.isOpen():
+                self.ser.open()
+            else:
+                self.ser.setDTR(False)
+                self.ser.flushInput()
+                self.ser.setDTR(True)
+                self.ser.reset_input_buffer()
+                self.connection_state = True
+                self.received_data = None
+                print("Serial Initialized")
         except Exception:
             print("Failed Initialization Serial")
-        
 
     def get_value(self):
         print("0")
-        if self.connection_state == True:
+        if self.connection_state:
             print("1")
             try:
                 print("2")
-                if self.ser.in_waiting > 0:
-                    print("3")
-                    self.received_data= self.ser.read().decode('utf-8')
-                    print("Debug Receive: ", self.received_data)
-                    return self.received_data
+                self.ser.flushOutput()
+                self.ser.write(self.serial_command.encode(encoding='utf-8'))
+                print("3")
+                self.received_data = self.ser.readline().decode('utf-8')
+                print("Debug Receive: ", self.received_data)
+                return self.received_data
                 
-            except Exception :
+            except Exception:
                 print("4")
                 print("Serial Closed")
                 self.connection_state = False
-                self.ser.close()
-        else :
+
+        else:
             print("5")
-            self.__init__(port=self.port, baudrate=self.baudrate, timeout=self.timeout)
-            print ("Restart Serial")
+            self.__init__(port=self.port, parity=self.parity, stopbits=self.stopbits, bytesize=self.bytesize,
+                          baudrate=self.baudrate, timeout=self.timeout)
+            print("Restart Serial")
