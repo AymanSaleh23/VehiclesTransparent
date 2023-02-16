1ca5bde730407f1cd0d903de99649436e1b3f9da
Ayman Saleh <ayman.saleh.zien@gmail.com>
2023-02-16 18:16:53 +0200

    solve a bug with class attribute {connection_state}

--- comlib/com_serial.py
+++ comlib/com_serial.py
@@ -6,9 +6,14 @@ class SerialComm:
         self.port = port
         self.baudrate = baudrate
         self.timeout = timeout
+        self.connection_state = False
         try:
             self.ser = serial.Serial(self.port, self.baudrate, timeout=self.timeout)
-            time.sleep(3)
+            self.ser.setDTR(False)
+            time.sleep(1)
+            self.ser.flushInput()
+            self.ser.setDTR(True)
+            time.sleep(2)
             self.ser.reset_input_buffer()
             self.connection_state = True
             self.received_data = None
@@ -25,7 +30,7 @@ class SerialComm:
                 print("2")
                 if self.ser.in_waiting > 0:
                     print("3")
-                    self.received_data= self.ser.readline().decode('utf-8')
+                    self.received_data= self.ser.read().decode('utf-8')
                     print("Debug Receive: ", self.received_data)
                     return self.received_data
                 
