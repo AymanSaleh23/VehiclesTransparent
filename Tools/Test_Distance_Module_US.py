cc22d3737c8c1cafe887d4be27b5076a52637774
Ayman Saleh <ayman.saleh.zien@gmail.com>
2023-02-16 13:54:20 +0000

    Create Test_Distance_Module_US.py
    Test file for Distance module which is work by Ultrasonic Sensor HC-SR04

---
+++ Tools/Test_Distance_Module_US.py
@@ -0,0 +1,9 @@
+u1 = Measure(27, 22)
+u2 = Measure(23, 24)
+u3 = Measure(5, 6)
+
+while True:
+      print(f"u1: {u1.distance_read()}cm")
+      print(f"u2: {u2.distance_read()}cm")
+      print(f"u3:{ u3.distance_read()}cm")
+      time.sleep(0.1)
