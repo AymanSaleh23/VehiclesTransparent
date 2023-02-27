d3dfbc58a2da06ef3c2a98e20a840500b1ac832e
Ayman Saleh <ayman.saleh.zien@gmail.com>
2023-02-27 23:59:33 +0200

    Initial integration for back vehicle

--- front_app/main.py
+++ front_app/main.py
@@ -3,7 +3,7 @@ import time
 from threading import Thread
 
 if __name__ == "__main__":
-    t_cv = Thread(target=os.system, args=["python cv_app.py"])
+    t_cv = Thread(target=os.system, args=["python main.py"])
     t_m = Thread(target=os.system, args=["python measure_app.py"])
 
     t_cv.setDaemon(True)
